a7f33618cc4b8f7504f0d5482aff3bc30fd10d0b
Santiago Sinatra <santiagosinatra@gmail.com>
2021-01-20 13:19:31 -0300

    replaced static text  with dynamic functionality (increase function created)

--- project-1/index.js
+++ project-1/index.js
@@ -1,15 +1,23 @@
 import { createApp } from './node_modules/vue/dist/vue.esm-browser.js';
 
 const app = createApp({
-  template: `Hello {{ person.name }} you have {{ person.age }} years old <!-- Los objetos que contenga data() se interpolan -->`,
+  template: `
+  <button v-on:click="increment(5)">Increment</button>
+  <!-- El v-on es como el de jquery o el onclick de javascript -->
+
+  <p> {{ count }}</p> 
+  <!-- Los objetos que contenga data() se interpolan y se actualizan automaticamente cuando cambia data -->`, /* Un template contiene el html que se va a ver */
 
   data() { /* Data devuelve objetos */
     return {
-      msg: 'world',
-      person: {
-        name: "Santiago",
-        age: 23
-      }
+      count: 0
+    }
+  },
+  methods: {
+    increment(value) {
+      this.count += value;
     }
   }
 }).mount('#app'); /* Creo mi app en Vue y la monto sobre el div con el id "app" */
+
+window.app = app;
