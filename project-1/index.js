abee1bd34e980e3e8d2fea0e8b941fe354d3ed87
Santiago Sinatra <santiagosinatra@gmail.com>
2021-01-21 15:51:21 -0300

    added styles that go along with new logic

--- project-1/index.js
+++ project-1/index.js
@@ -2,7 +2,7 @@ import { createApp } from './node_modules/vue/dist/vue.esm-browser.js';
 
 const app = createApp({
   template: `
-  <button v-on:click="increment(5)">Increment</button>
+  <button v-on:click="increment(5)">Increment</button> <!--Esto tambien se puede escribir como @click= -->
   <!-- El v-on es como el de jquery o el onclick de javascript -->
 
   <p> {{ count }}</p> 
@@ -15,24 +15,35 @@ const app = createApp({
     Odd
   </div>
 
-  <div v-for="number in evenList">
+  <div v-for="number in numbers">
     <!-- <div v-show="isEven(number)">  El v-show hace casi lo mismo que el v-if pero crea el div con un display: none -->
-    {{ number }}
+    <div v-bind:class="getClass(number)" v-bind:title="number"> <!-- El v-bind me permite de alguna forma conectar el vue con el html -->
+      {{ number }}
+    </div>
     <!-- </div> -->
   </div>
 
+  <input @input="input" v-bind:value="value" />
+  <div v-if="error"> {{ error }} </div>
+
   `, /* Un template contiene el html que se va a ver */
 
-  computed: { /* Vendrian a ser como propiedades inteligentes que se actualizan continuamente si hay un cambio */
+  computed: { /* Vendrian a ser como propiedades inteligentes que se actualizan continuamente si hay un cambio no pueden llevar argumentos */
     evenList() {
       return this.numbers.filter(num => this.isEven(num));
+    },
+    error() {
+      if (this.value.length < 7) { 
+        return "Too short";
+      }
     }
   },
 
   data() { /* Data devuelve objetos */
     return {
       count: 0,
-      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      value: "User",
     }
   },
 
@@ -42,6 +53,16 @@ const app = createApp({
     },
     isEven(number){
       return number % 2 === 0
+    },
+    getClass(number){
+      if(this.isEven(number)) {
+        return 'red'
+      } else {
+        return 'blue'
+      }
+    },
+    input($evt){
+      this.value = $evt.target.value;
     }
   }
 }).mount('#app'); /* Creo mi app en Vue y la monto sobre el div con el id "app" */
