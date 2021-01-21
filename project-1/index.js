df694b6ade4f0844c3bbabd9366a16b252cd0f42
Santiago Sinatra <santiagosinatra@gmail.com>
2021-01-21 13:06:59 -0300

    updated logic with new v-if, v-for, v-show properties and added computed elements

--- project-1/index.js
+++ project-1/index.js
@@ -6,16 +6,42 @@ const app = createApp({
   <!-- El v-on es como el de jquery o el onclick de javascript -->
 
   <p> {{ count }}</p> 
-  <!-- Los objetos que contenga data() se interpolan y se actualizan automaticamente cuando cambia data -->`, /* Un template contiene el html que se va a ver */
+  <!-- Los objetos que contenga data() se interpolan y se actualizan automaticamente cuando cambia data -->
+
+  <div v-if="isEven(count)">
+    Even
+  </div>
+  <div v-else> <!-- El v-else tiene que estar pegado al v-if para que funcione -->
+    Odd
+  </div>
+
+  <div v-for="number in evenList">
+    <!-- <div v-show="isEven(number)">  El v-show hace casi lo mismo que el v-if pero crea el div con un display: none -->
+    {{ number }}
+    <!-- </div> -->
+  </div>
+
+  `, /* Un template contiene el html que se va a ver */
+
+  computed: { /* Vendrian a ser como propiedades inteligentes que se actualizan continuamente si hay un cambio */
+    evenList() {
+      return this.numbers.filter(num => this.isEven(num));
+    }
+  },
 
   data() { /* Data devuelve objetos */
     return {
-      count: 0
+      count: 0,
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     }
   },
-  methods: {
+
+  methods: { /* Contiene la lógica de la página osea las funciones y eso */
     increment(value) {
       this.count += value;
+    },
+    isEven(number){
+      return number % 2 === 0
     }
   }
 }).mount('#app'); /* Creo mi app en Vue y la monto sobre el div con el id "app" */
