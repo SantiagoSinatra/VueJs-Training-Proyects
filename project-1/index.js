1d2fa379380c1d97cf6c3ec6091dfcca74d1ca82
Santiago Sinatra <santiagosinatra@gmail.com>
2021-01-27 11:59:22 -0300

    moved some functionality of the numbers to a separate component. Deleted other stuff in order to follow the tutorial

--- project-1/index.js
+++ project-1/index.js
@@ -1,37 +1,43 @@
 import { createApp } from './node_modules/vue/dist/vue.esm-browser.js';
 
-const Hello = { /* Componente separado que puede ser utilizado en cualquier parte del código */
-  template: `<h3>Hello from component</h3>`
+const Num = { /* Componente separado que puede ser utilizado en cualquier parte del código */
+  props: { /* Propiedades que va a recibir del padre en el que se lo incluya */
+    theNumber: {
+      type: Number,
+      required: true
+    }
+  },
+  template: `<div v-bind:class="getClass(theNumber)"> {{ theNumber }} </div>`,
+  methods: {
+    getClass(number){
+      if(this.isEven(number)) {
+        return 'red'
+      } else {
+        return 'blue'
+      }
+    },
+    isEven(number){
+      return number % 2 === 0
+    }
+  }
 }
 
 const app = createApp({
   components: { /* Declaro los componentes que voy a usar */
-    Hello
+    Num
   },
 
   template: `
-  <hello /> <!-- importo el componente que creé y declaré más arriba -->
   <button v-on:click="increment(5)">Increment</button> <!--Esto tambien se puede escribir como @click= -->
   <!-- El v-on es como el de jquery o el onclick de javascript -->
 
   <p> {{ count }}</p> 
-  <!-- Los objetos que contenga data() se interpolan y se actualizan automaticamente cuando cambia data -->
+  <!-- Los objetos que contenga data() se interpolan y se actualizan automaticamente cuando cambia data --> 
 
-  <div v-if="isEven(count)">
-    Even
-  </div>
-  <div v-else> <!-- El v-else tiene que estar pegado al v-if para que funcione -->
-    Odd
-  </div>
-
-  <div v-for="number in numbers">
-    <!-- <div v-show="isEven(number)">  El v-show hace casi lo mismo que el v-if pero crea el div con un display: none -->
-    <div v-bind:class="getClass(number)" v-bind:title="number"> <!-- El v-bind me permite de alguna forma conectar el vue con el html -->
-      {{ number }}
-    </div>
-    <!-- </div> -->
-  </div>
-  <hello /> <!-- importo denuevo el componente que creé y declaré más arriba -->
+  
+  <num v-for="number in numbers" v-bind:theNumber="number" /> <!-- En esta linea estoy bindeando a la variable theNumber el valor que tiene number, que viene de data a mi componente separado Num -->
+  
+  
   <!-- <input @input="input" v-bind:value="value" /> -->
   <input type="checkbox" v-model="value" value="a"/>  <!-- Esto hace lo mismo que lo de arriba -->
   <input type="checkbox" v-model="value" value="b"/>
@@ -43,9 +49,6 @@ const app = createApp({
   `, /* Un template contiene el html que se va a ver */
 
   computed: { /* Vendrian a ser como propiedades inteligentes que se actualizan continuamente si hay un cambio no pueden llevar argumentos */
-    evenList() {
-      return this.numbers.filter(num => this.isEven(num));
-    },
     error() {
       if (this.value.length < 7) { 
         return "Too short";
@@ -64,16 +67,6 @@ const app = createApp({
   methods: { /* Contiene la lógica de la página osea las funciones y eso */
     increment(value) {
       this.count += value;
-    },
-    isEven(number){
-      return number % 2 === 0
-    },
-    getClass(number){
-      if(this.isEven(number)) {
-        return 'red'
-      } else {
-        return 'blue'
-      }
     },
     input($evt){
       this.value = $evt.target.value;
