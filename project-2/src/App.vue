97e70f80d29c32ac44d6658a3902cfabac8aa24d
Santiago Sinatra <santiagosinatra@gmail.com>
2021-01-28 16:06:13 -0300

    Last commit of this project, added form tag and shorthand for prevent default

--- project-2/src/App.vue
+++ project-2/src/App.vue
@@ -1,26 +1,28 @@
 <template>
   <div>
-    <my-input 
-      name="Username"
-      :rules = "{ required: true, min: 5 }"
-      :value = "username.value"
-      type = "text"
-      @update="update"
-    />
+    <form @submit.prevent="submit" class="form"> <!-- @submit tiene un modificador que previene el comportamiento por defecto de en este caso el formulario. Es un shorthand para $evt.preventDefault() -->
+      <my-input 
+        name="Username"
+        :rules = "{ required: true, min: 5 }"
+        :value = "username.value"
+        type = "text"
+        @update="update"
+      />
 
-    <my-input 
-      name="Password"
-      :rules = "{ required: true, min: 8 }"
-      :value = "password.value"
-      type = "password"
-      @update="update"
-    />
+      <my-input 
+        name="Password"
+        :rules = "{ required: true, min: 8 }"
+        :value = "password.value"
+        type = "password"
+        @update="update"
+      />
 
-    <my-button 
-      color="white"
-      background="darkslateblue"
-      :disabled="!valid"
-    /> <!-- : es el shorthand para v-bind lo usé para bindear una variable a una propiedad-->
+      <my-button 
+        color="white"
+        background="darkslateblue"
+        :disabled="!valid"
+      /> <!-- : es el shorthand para v-bind lo usé para bindear una variable a una propiedad-->
+    </form>
   </div> <!-- Agregué este div para que el eslint no me hinche con que tiene que haber un solo componente... -->
 </template>
 
@@ -53,6 +55,9 @@ export default {
     }
   },
   methods: { 
+    submit($evt){
+      /* $evt.preventDefault(); */
+    },
     update(payload) {
       this[payload.name.toLowerCase()] = { /* Lowercaseo el value */
         value: payload.value, /* lo guardo en los inputs */
@@ -66,6 +71,11 @@ export default {
 <style>
 body {
   font-family: Arial, Helvetica, sans-serif;
-  padding: 10px 50px;
+}
+
+.form {
+  margin: 0 auto;
+  max-width: 400px;
+  width: 50%;
 }
 </style>
