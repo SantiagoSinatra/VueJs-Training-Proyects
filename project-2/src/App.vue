ad3540957987a4f9bfdba20fb2ad67dd9de3603f
Santiago Sinatra <santiagosinatra@gmail.com>
2021-01-28 14:58:33 -0300

    added inputs with customizable validation rules using v-bind and v-model mainly with a computed function

--- project-2/src/App.vue
+++ project-2/src/App.vue
@@ -1,24 +1,42 @@
 <template>
-  <my-button 
-    color="white"
-    background="darkslateblue"
-    :disabled="!valid"
-  /> <!-- : es el shorthand para v-bind lo usé para bindear una variable a una propiedad-->
+  <div>
+    <my-input 
+      name="Username"
+      :rules = "{ required: true, min: 5 }"
+    />
+    <my-input 
+      name="Password"
+      :rules = "{ required: true, min: 8 }"
+    />
+    <my-button 
+      color="white"
+      background="darkslateblue"
+      :disabled="!valid"
+    /> <!-- : es el shorthand para v-bind lo usé para bindear una variable a una propiedad-->
+  </div> <!-- Agregué este div para que el eslint no me hinche con que tiene que haber un solo componente... -->
 </template>
 
 <script>
 import MyButton from './components/MyButton.vue'
+import MyInput from './components/MyInput.vue'
+
 export default {
   components: {
-    MyButton
+    MyButton,
+    MyInput,
+    MyInput
   },
   data() {
     return {
-      valid: false /* Esta es la variable que bindee arriba */
+      valid: true /* Esta es la variable que bindee arriba */
     }
   }
 }
 </script>
 
-<style scoped>
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  padding: 10px 50px;
+}
 </style>
---
+++ project-2/src/components/MyInput.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="input-wrapper">
+    <div class="label">
+      <label :for="name">{{ name }}</label>
+      <div class="error">{{ error }}</div> <!-- imprime el error que salga dependiendo el value que haya -->
+    </div>
+    <input
+      :id="name" 
+      type="text"
+      v-model="value" 
+    />
+    <!-- v-model: es el shorthand para v-bind:value="value" @input="value" = $event || basicamente y mágicamente escucha el valor que tenga el input apenas lo uso y va actualizando la variable que ponga en data() -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    rules: {
+      type: Object // min, required
+    }
+  },
+  data() {
+    return {
+      value: '' /* Variable del input que escucho con el v-model */
+    }
+  },
+  computed: {
+    error() {
+      /* Con ese value hago las validaciones correspondientes dentro de una funcion en computed que se ejecuta en cada instancia nueva (osea cada vez que cambia el value) */
+      /* console.log("me ejecuto") descomentar esta linea para ver como actua :D*/
+      if(this.rules.required && this.value.length === 0) {
+        return 'Required';
+      }
+      if(this.rules.min && this.value.length < this.rules.min) {
+        return `Minimum length is  ${this.rules.min}`;
+      }
+    }
+  }
+  
+}
+</script>
+
+<style scoped>
+.input-wrapper {
+  display: flex;
+  flex-direction: column;
+}
+
+.error {
+  color: red;
+}
+
+.label {
+  display: flex;
+  justify-content: space-between;
+}
+
+input {
+  margin: 5px 0;
+  padding: 10px;
+  font-size: 16px;
+  background: none;
+  color: black;
+  border: 1px solid silver;
+  border-radius: 5px;
+}
+</style>
