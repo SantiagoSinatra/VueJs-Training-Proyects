249f9f394e0ec823057e81fe95ef78d63f394d2b
Santiago Sinatra <santiagosinatra@gmail.com>
2021-01-28 13:03:12 -0300

    first commit on this second proyect | created new vite project using npm create-vite-app

---
+++ project-2/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <my-button 
+    color="white"
+    background="darkslateblue"
+    disabled="true"
+  />
+</template>
+
+<script>
+import MyButton from './components/MyButton.vue'
+export default {
+  components: {
+    MyButton
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ project-2/src/components/MyButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button v-bind:style="{ background: background, color}">
+    Button
+  </button> <!-- Por ejemplo acá estoy usando la variable background y el valor lo asigno en el padre App.vue --> <!-- Además si tengo el mismo nombre de variable que el nombre de propiedad puedo omitir el nombre de la propiedad como hice con el color -->
+</template>
+
+<script>
+export default {
+  props: { /* Propiedades que se exportan al padre y se leen en este componente (y se usan como variables para realizar algun comportamiento (ver arriba)) */
+    color: {
+      type: String
+    },
+    background: {
+      type: String
+    },
+    disabled: {
+      type: Boolean
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  background: none;
+  color: black;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 40px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+button:hover {
+  filter: brightness(125%);
+}
+</style>
